b48044072f6c2620a5c46d9318bc87b131fdee15
Pietro Carvalho Bueno <pietro.bueno@etec.sp.gov.br>
2026-05-27 16:17:26 -0300

    sql

--- produtos.sql
+++ produtos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13/05/2026 às 21:15
+-- Tempo de geração: 13/05/2026 às 21:13
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
